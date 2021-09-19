6ff666aa5531431cb198b7ea1883e5e9d0472462
Samuel Wittlinger <samo.wittlinger@gmail.com>
2021-09-19 20:21:57 +0200

    test

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import asyncio
 from http import server
+
+running = False
+
+KEEP_RUNNING = True
 
 
 class S(server.BaseHTTPRequestHandler):
@@ -13,22 +18,26 @@ class S(server.BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        global KEEP_RUNNING
         self._set_response()
         self.wfile.write("GET request for {}".format(
             self.path).encode('utf-8'))
+        KEEP_RUNNING = False
+
+
+def keep_running():
+    return KEEP_RUNNING
 
 
 def run(server_class=server.HTTPServer, handler_class=S):
     server_address = ('', 8080)
     httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+    while keep_running():
+        httpd.handle_request()
+    run_bot()
 
 
-if __name__ == '__main__':
-
-    load_dotenv()
-    run()
-
+def run_bot():
     cogs = [music]
 
     client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
@@ -37,3 +46,9 @@ if __name__ == '__main__':
         cogs[i].setup(client)
 
     client.run(os.getenv('DISCORD_BOT_TOKEN'))
+
+
+if __name__ == '__main__':
+
+    load_dotenv()
+    run()
